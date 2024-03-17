da19865e0b429da84d0c6d1c5fd5943955e3954a
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-03-17 19:43:32 +0530

    Initial project setup

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("@nomicfoundation/hardhat-toolbox");
+const { task } = require("hardhat/config");
+require("hardhat-docgen");
+require("dotenv").config();
+
+const cleanup = require("./tasks/cleanup/cleanup.js");
+
+task(
+    "cleanup",
+    "Remove giveaway from consumers, cancel subscription and withdraw any remaining LINK that the contract holds",
+)
+    .addParam("address", "The address of the deployed giveaway contract")
+    .setAction((taskArgs, hre) => cleanup(taskArgs, hre));
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const REPORT_GAS = process.env.REPORT_GAS || false;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const PRIVATE_ACCOUNT_1 =
+    process.env.PRIVATE_ACCOUNT_1 ||
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+const PRIVATE_ACCOUNT_2 =
+    process.env.PRIVATE_ACCOUNT_2 ||
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.20",
+            },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+        // example configuration for the Sepolia testnet
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_ACCOUNT_1, PRIVATE_ACCOUNT_2],
+            chainId: 11155111,
+        },
+    },
+    gasReporter: {
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        enabled: REPORT_GAS || false,
+        outputFile: "gasReport.txt",
+        currency: "USD",
+        showTimeSpent: true,
+        noColors: true,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    docgen: {
+        path: "./docs",
+        clear: true, //clears the content of the ouptut directory (in this  case, ./docs before generating new docs)
+        runOnCompile: false,
+    },
+    mocha: {
+        timeout: 200000, //200 seconds
+    },
+};
---
+++ helper.config.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+
+const developmentChainIds = [31337];
+const networkConfig = {
+    // this configuration for the Sepolia testnet is provided as an example
+    11155111: {
+        name: "sepolia",
+        blockConfirmations: 3,
+        keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 150 gwei keyHash
+        callbackGasLimit: "500000", // the maximum gas limit for fulfillRandomWords function
+        interval: "10", // the amount of time after which the giveaway winner will be selected (here, its set to 10 seconds)
+        vrfCoordinatorAddress: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        linkTokenAddress: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+        upkeepContractAddress: "0xb0E49c5D0d05cbc241d68c05BC5BA1d1B7B72976",
+        fundLinkAmountForSubscription: ethers.parseEther("12"),
+        fundLinkAmountForUpkeep: ethers.parseEther("5"),
+    },
+    31337: {
+        name: "localhost",
+        blockConfirmations: 1,
+        subscriptionId: 1,
+        keyHash: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "5000000",
+        interval: "5",
+    },
+};
+
+module.exports = {
+    developmentChainIds,
+    networkConfig,
+};
